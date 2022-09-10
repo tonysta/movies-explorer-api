4fe2ba3214f2a3c31a8a056a06998c08c52cf885
anton startsev <anton.startsev@osome.com>
2022-09-10 22:52:23 +0300

    body-parser

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { usersRouter } = require('./routes/users');
+const { moviesRouter } = require('./routes/movies');
 
 const { PORT = 3000 } = process.env;
 
@@ -8,6 +11,14 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
