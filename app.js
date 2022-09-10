38d8016783541d44908a1cc37843be1e756bc1b8
anton startsev <anton.startsev@osome.com>
2022-09-10 23:14:16 +0300

    add logger

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { usersRouter } = require('./routes/users');
 const { moviesRouter } = require('./routes/movies');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -17,10 +18,17 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
+app.use(requestLogger);
+
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
 
+app.use((req, res, next) => {
+  next(new NotFoundError('Cтраницы не существует'));
+});
+
+app.use(errorLogger);
+
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
