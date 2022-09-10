96765b8299ba27b8b0659655830afea74bc49226
anton startsev <anton.startsev@osome.com>
2022-09-11 02:12:05 +0300

    auth + validation

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, Joi, errors } = require('celebrate');
+const cors = require('cors');
 const { usersRouter } = require('./routes/users');
 const { moviesRouter } = require('./routes/movies');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { handleError } = require('./middlewares/handleError');
 const { NotFoundError } = require('./utils/errors/notFound');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
@@ -21,7 +25,22 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(requestLogger);
+app.use(cors());
 
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+app.use(auth);
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
 
@@ -30,6 +49,8 @@ app.use((req, res, next) => {
 });
 
 app.use(errorLogger);
+
+app.use(errors());
 
 app.use(handleError);
 
--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { UnauthorizedError } = require('../utils/errors/unauthorizedError');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -24,5 +26,22 @@ const userSchema = new mongoose.Schema({
     maxlength: [30, 'Необходимо ввести 30 или менее символов, вы ввели {VALUE}'],
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
